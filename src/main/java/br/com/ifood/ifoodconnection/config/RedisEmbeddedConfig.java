1f30df3c89597d8d405134dbebdc56d46ed6377b
Weliff Lima <weliff.14@gmail.com>
2018-04-16 03:46:44 -0300

    Adicionado Config Redis Embedded

---
+++ src/main/java/br/com/ifood/ifoodconnection/config/RedisEmbeddedConfig.java
@@ -0,0 +1,29 @@
+package br.com.ifood.ifoodconnection.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Configuration
+public class RedisEmbeddedConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void start() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stop() {
+        redisServer.stop();
+    }
+}
