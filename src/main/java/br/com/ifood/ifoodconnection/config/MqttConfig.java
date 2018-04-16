2c5addb11f9499c66fa10c3b33dd3cbe65f68657
Weliff Lima <weliff.14@gmail.com>
2018-04-16 04:28:30 -0300

    Adicionado configuração mqtt

---
+++ src/main/java/br/com/ifood/ifoodconnection/config/MqttConfig.java
@@ -0,0 +1,48 @@
+package br.com.ifood.ifoodconnection.config;
+
+import br.com.ifood.ifoodconnection.handler.RestaurantConnectionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+
+@Configuration
+@EnableConfigurationProperties(IfoodMqttProperty.class)
+public class MqttConfig {
+
+    @Autowired
+    private IfoodMqttProperty mqttProperty;
+
+    @Bean
+    public IntegrationFlow mqttInFlow(RestaurantConnectionHandler restaurantConnectionHandler) {
+        return IntegrationFlows.from(mqttInbound())
+                .handle(restaurantConnectionHandler.handle())
+                .get();
+    }
+
+    @Bean
+    public MessageProducerSupport mqttInbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(mqttProperty.getClientId(),
+                mqttClientFactory(), mqttProperty.getTopics());
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        return adapter;
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setServerURIs(mqttProperty.getUrl());
+        factory.setUserName(mqttProperty.getUsername());
+        factory.setPassword(mqttProperty.getPassword());
+        return factory;
+    }
+}
