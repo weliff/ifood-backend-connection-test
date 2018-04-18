78968c2074158749ad05328b411c81bd791663e8
Weliff Lima <weliff.14@gmail.com>
2018-04-18 04:52:03 -0300

    Adicionado Client Simulator

---
+++ src/main/java/br/com/ifood/ifoodconnection/ClientSimulator.java
@@ -0,0 +1,39 @@
+package br.com.ifood.ifoodconnection;
+
+import br.com.ifood.ifoodconnection.model.ConnectionState;
+import br.com.ifood.ifoodconnection.model.dto.RestaurantStateDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class ClientSimulator {
+
+    public static void main(String[] args) throws MqttException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        RestaurantStateDTO restaurantConnectionOnline = new RestaurantStateDTO(1L, ConnectionState.ONLINE);
+        byte[] onlineMessage = objectMapper.writeValueAsBytes(restaurantConnectionOnline);
+
+        RestaurantStateDTO restaurantConnectionOffline = new RestaurantStateDTO(1L, ConnectionState.OFFLINE);
+        byte[] offlineMessage = objectMapper.writeValueAsBytes(restaurantConnectionOffline);
+
+
+        MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+
+        mqttConnectOptions.setKeepAliveInterval(2);
+
+        mqttConnectOptions.setWill("connectionLost", offlineMessage, 1, false);
+
+        client.connect(mqttConnectOptions);
+        MqttMessage message = new MqttMessage();
+        message.setPayload(onlineMessage);
+        message.setQos(1);
+
+        client.publish("connectionActive", message);
+
+    }
+}
--- src/main/java/br/com/ifood/ifoodconnection/IfoodConnectionApplication.java
+++ src/main/java/br/com/ifood/ifoodconnection/IfoodConnectionApplication.java
@@ -1,30 +1,15 @@
 package br.com.ifood.ifoodconnection;
 
-import br.com.ifood.ifoodconnection.model.ConnectionState;
 import br.com.ifood.ifoodconnection.model.Restaurant;
-import br.com.ifood.ifoodconnection.model.RestaurantState;
 import br.com.ifood.ifoodconnection.repository.RestaurantRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.handler.codec.mqtt.MqttQoS;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.Pollers;
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
-import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
-import org.springframework.messaging.MessageHandler;
 
-import java.time.Duration;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
@@ -54,10 +39,10 @@ public class IfoodConnectionApplication {
 //            restaurantRepository.findAll().forEach(restaurant -> {
 //                try {
 //
-//                    RestaurantState restaurantConnectionOnline = new RestaurantState(restaurant.getId(), ConnectionState.ONLINE);
+//                    RestaurantStateDTO restaurantConnectionOnline = new RestaurantStateDTO(restaurant.getId(), ConnectionState.ONLINE);
 //                    byte[] onlineMessage = objectMapper.writeValueAsBytes(restaurantConnectionOnline);
 //
-//                    RestaurantState restaurantConnectionOffline = new RestaurantState(restaurant.getId(), ConnectionState.OFFLINE);
+//                    RestaurantStateDTO restaurantConnectionOffline = new RestaurantStateDTO(restaurant.getId(), ConnectionState.OFFLINE);
 //                    byte[] offlineMessage = objectMapper.writeValueAsBytes(restaurantConnectionOffline);
 //
 //                    MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
