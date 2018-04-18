92631f6302865fb48443f7a0588956962e1de7bc
Weliff Lima <weliff.14@gmail.com>
2018-04-18 04:22:25 -0300

    Adicionado Exchanges config

--- src/main/java/br/com/ifood/ifoodconnection/config/CacheConfig.java
+++ src/main/java/br/com/ifood/ifoodconnection/config/CacheConfig.java
@@ -1,21 +1,12 @@
 package br.com.ifood.ifoodconnection.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Profile("!test")
---
+++ src/main/java/br/com/ifood/ifoodconnection/config/Exchanges.java
@@ -0,0 +1,7 @@
+package br.com.ifood.ifoodconnection.config;
+
+public interface Exchanges {
+
+    String RESTAURANT_STATUS_CHANGE = "restaurant.status.change";
+
+}
