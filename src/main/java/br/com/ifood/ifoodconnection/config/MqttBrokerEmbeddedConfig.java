68f43854e491e4e0de9560d01141cff3066ce982
Weliff Lima <weliff.14@gmail.com>
2018-04-18 04:21:52 -0300

    Adicionado Embbeded Mqtt Broker

---
+++ src/main/java/br/com/ifood/ifoodconnection/config/MqttBrokerEmbeddedConfig.java
@@ -0,0 +1,32 @@
+package br.com.ifood.ifoodconnection.config;
+
+import io.moquette.server.Server;
+import io.moquette.server.config.ClasspathResourceLoader;
+import io.moquette.server.config.IConfig;
+import io.moquette.server.config.IResourceLoader;
+import io.moquette.server.config.ResourceLoaderConfig;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Profile("!test")
+//@Configuration
+public class MqttBrokerEmbeddedConfig {
+
+    private Server serverMqtt;
+
+    @PostConstruct
+    public void start() throws IOException {
+        IResourceLoader classpathLoader = new ClasspathResourceLoader();
+        IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+        this.serverMqtt = new Server();
+        this.serverMqtt.startServer(classPathConfig);
+    }
+
+    @PreDestroy
+    public void stop() {
+        serverMqtt.stopServer();
+    }
+}
